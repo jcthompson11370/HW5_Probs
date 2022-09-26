a2ca4f25580590bdd114050e72568e54b2607255
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-09-26 05:42:27 +0000

    initialized variables and coded input for #of lawns and $ of seed

---
+++ HW5_P5/main5.cpp
@@ -0,0 +1,25 @@
+// Programmer: John Thompson
+// Description: HW5_P5
+// Date: 09 / 21 / 2022
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  // initialize variables
+  int NumOfLawns; 
+  double SeedCost;
+  double totalArea; //unknown variable
+
+  //input variables
+  cout << "input number of lawns: " << endl;
+  cin >> NumOfLawns;
+
+  cout << "input cost of seed: " << endl;
+  cin >> SeedCost;
+  
+  return 0;
+}
