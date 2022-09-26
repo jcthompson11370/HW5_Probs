9d8edad881f66d3e0994856b923688118da9afd7
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-09-26 05:11:09 +0000

    final commit P4

--- HW5_P4/main4.cpp
+++ HW5_P4/main4.cpp
@@ -13,16 +13,16 @@ int main() {
 
   cout << "Enter ten number values: " << endl;
   
-    for (int i = 0; i < 42; i++) 
+    for (int i = 0; i < 42; i++) // "for" loop for bool modulo [42] value
     {
-        modulo [i] = false;
+        modulo [i] = false;  
     }
 
-    for (int k = 0; k < 10; k++)
+    for (int k = 0; k < 10; k++) // use mod math with number inputs to output a true statement with bool modulo [42]
       {
         int num_values;
         
-        cin >> num_values;
+        cin >> num_values; // input ten values into for loop for mod math
         
         modulo [num_values % 42] = true;
       }
@@ -31,7 +31,7 @@ int main() {
 
     for (int g = 0; g < 42; g++) 
       {
-        if (modulo [g]) 
+        if (modulo [g]) // if modulo is less than 42, then the distinct value adds up until it reaches limit in for loop
         {
           dist_value++;
         }
